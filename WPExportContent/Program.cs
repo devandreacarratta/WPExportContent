dd2a0e1c724173a016d926170aca53e287e6cd5f
Andrea Carratta <andrea.carratta.dev@outlook.it>
2019-11-09 14:12:54 +0100

    Init Commit
    Add Empty Project and Solution

---
+++ WPExportContent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WPExportContent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
