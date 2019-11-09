a8e2024903a20f659c4202e97134a2cbfac13302
Andrea Carratta <andrea.carratta.dev@outlook.it>
2019-11-09 18:10:55 +0100

    GetMySQLConnectionString - Add Port (3306 as default)

--- WPExportContent.Core/Helper.cs
+++ WPExportContent.Core/Helper.cs
@@ -3,9 +3,9 @@
     public class Helper
     {
 
-        public static string GetMySQLConnectionString(string server, string database, string uid, string pwd)
+        public static string GetMySQLConnectionString(string server, string database, string uid, string pwd,int port = 3306)
         {
-            return $"Server={server};Database={database};Uid={uid};Pwd={pwd};";
+            return $"Server={server};Database={database};Uid={uid};Pwd={pwd};Port={port}";
         }
 
     }
