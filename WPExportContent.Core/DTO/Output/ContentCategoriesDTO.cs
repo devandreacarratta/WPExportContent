1573a34eace1ab5ba934e1cd8e1d1230c8178bab
Andrea Carratta <46504271+devandreacarratta@users.noreply.github.com>
2019-11-16 11:58:12 +0100

    Move Tags / Categories  outside Contents (#6)

---
+++ WPExportContent.Core/DTO/Output/ContentCategoriesDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace WPExportContent.Core.DTO.Output
+{
+    public class ContentCategoriesDTO : BaseOutputDTO
+    {
+        public long IDContent { get; set; }
+        public long IDCategory { get; set; }
+    }
+}
