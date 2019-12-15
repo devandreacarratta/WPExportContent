4536cd01b1f788f4e5cb04ea982dc4b80ed9728f
Andrea Carratta <andrea.carratta.dev@outlook.it>
2019-12-15 16:20:39 +0100

    WPExportContent.Core - Remove using

--- WPExportContent.Core/Export/ExportToDatabase.cs
+++ WPExportContent.Core/Export/ExportToDatabase.cs
@@ -1,7 +1,6 @@
 ﻿using Dapper;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
 using System.Threading.Tasks;
 using WPExportContent.Core.DataAccess;
 using WPExportContent.Core.DataAccess.Queries;
